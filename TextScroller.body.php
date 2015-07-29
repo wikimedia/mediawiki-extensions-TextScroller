f644523c58e0294c611eb53f2ba83a1c80817947
Jack Phoenix <jack@countervandalism.net>
2015-07-29 12:34:15 +0300

    Initial import of a new extension from wikiHow
    With some improvements by yours truly for compatibility with MW 1.25+.
    
    Change-Id: I07f63401ab729b529af21c4a835c24d8a17eaa10

---
+++ TextScroller.body.php
@@ -0,0 +1,54 @@
+<?php
+
+class TextScroller {
+
+	public static function setParserFunction( &$parser ) {
+		# Setup parser hook
+		$parser->setFunctionHook( 'txtscrl', 'TextScroller::parserFunction' );
+		return true;
+	}
+
+	public static function parserFunction( $parser, $arrowText = '', $grayText = '', $scrollText = '' ) {
+		$scrollText = self::prepareText( $scrollText );
+		$grayText = self::prepareText( $grayText );
+		$arrowText = self::prepareText( $arrowText );
+
+		$parser->getOutput()->addModules( 'ext.textscroller.scripts' );
+		$parser->getOutput()->addModuleStyles( 'ext.textscroller.styles' );
+
+		if ( !empty( $arrowText ) ) {
+			$outerContent = "\t\t<div class=\"arrow_right\"></div>\n\t\t<div class=\"arrow_text\">{$arrowText}</div>\n";
+		} else {
+			$outerContent = "\n\t\t&nbsp;\n";
+		}
+
+		$id = 'scrl-' . hash( 'md5', $scrollText . mt_rand( 1, 1000 ) );
+		$html = <<<TEXTSCROLLER
+<div class="textscroller_outer" id="{$id}">
+	<div class="textscroller_inner">
+		<div class="greytext">{$grayText}</div>
+		<div class="scrolltext">{$scrollText}</div>
+	</div>
+	<div class="arrow_outer">
+		{$outerContent}
+	</div>
+</div>
+TEXTSCROLLER;
+		$html = preg_replace( "@\n@", '', $html );
+
+		return $parser->insertStripItem( $html );
+	}
+
+	private static function prepareText( $text ) {
+		$text = trim( $text );
+		// Replace line breaks with a token to later be replaced in the scroller JavaScript
+		$text = preg_replace( "@[\r\n]@", '@br@', $text );
+		return $text;
+	}
+
+	public static function languageGetMagic( &$magicWords ) {
+		$magicWords['txtscrl'] = array( 0, 'txtscrl' );
+		return true;
+	}
+
+}
---
+++ TextScroller.php
@@ -0,0 +1,31 @@
+<?php
+
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'txtscrl',
+	'version' => '1.0',
+	'license-name' => 'GPL-2.0+',
+	'author' => 'Jordan Small',
+	'descriptionmsg' => 'textscroller-desc',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:TextScroller',
+);
+
+// ResourceLoader support for MediaWiki 1.17+
+$wgResourceModules['ext.textscroller.styles'] = array(
+	'styles' => 'textscroller.css',
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'TextScroller',
+	'position' => 'top'
+);
+
+$wgResourceModules['ext.textscroller.scripts'] = array(
+	'scripts' => 'textscroller.js',
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'TextScroller'
+);
+
+$wgAutoloadClasses['TextScroller'] = __DIR__ . '/TextScroller.body.php';
+$wgMessagesDirs['TextScroller'] = __DIR__ . '/i18n';
+
+$wgHooks['LanguageGetMagic'][] = 'TextScroller::languageGetMagic';
+$wgHooks['ParserFirstCallInit'][] = 'TextScroller::setParserFunction';
