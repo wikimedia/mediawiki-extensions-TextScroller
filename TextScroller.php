5e30c68f0eb19ed6ea97292ec2e21bbd58d1352e
Reedy <reedy@wikimedia.org>
2018-07-11 03:00:50 +0100

    Empty out PHP entry point, replace with boilerplate
    Change-Id: I8fc7365e6e99b1ef17b1b131d72a8d27556882e7

--- TextScroller.php
+++ TextScroller.php
@@ -1,30 +1,15 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'txtscrl',
-	'version' => '1.0',
-	'license-name' => 'GPL-2.0-or-later',
-	'author' => 'Jordan Small',
-	'descriptionmsg' => 'textscroller-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:TextScroller',
-);
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'TextScroller' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['TextScroller'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for TextScroller extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the TextScroller extension requires MediaWiki 1.25+' );
+}
 
-// ResourceLoader support for MediaWiki 1.17+
-$wgResourceModules['ext.textscroller.styles'] = array(
-	'styles' => 'textscroller.css',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'TextScroller'
-);
-
-$wgResourceModules['ext.textscroller.scripts'] = array(
-	'scripts' => 'textscroller.js',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'TextScroller'
-);
-
-$wgAutoloadClasses['TextScroller'] = __DIR__ . '/TextScroller.body.php';
-$wgMessagesDirs['TextScroller'] = __DIR__ . '/i18n';
-
-$wgHooks['LanguageGetMagic'][] = 'TextScroller::languageGetMagic';
-$wgHooks['ParserFirstCallInit'][] = 'TextScroller::setParserFunction';
